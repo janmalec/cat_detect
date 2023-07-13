60cf1758c141f350821411b09da6992fce638384
Jan Malec <jan.malec@gmail.com>
2023-07-13 15:53:41 +0200

    first commit

---
+++ monitor_cat.py
@@ -0,0 +1,136 @@
+# Import the necessary modules
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import argparse
+from PIL import Image
+import cv2
+from datetime import datetime
+import os
+import emoji
+import time
+import requests
+
+def check_cat(frame):
+    # Load the pre-trained model (ResNet-18) and set it to evaluation mode
+    #model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+    # try ResNet-50
+    model = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
+    model.eval()
+
+    # Define the transform to resize and normalize the image
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    # Load the image from the file name given by argparse and apply the transform
+    image = Image.fromarray(frame)
+    image = transform(image)
+
+    # Add a batch dimension and move the image to the device (CPU or GPU)
+    image = image.unsqueeze(0)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    image = image.to(device)
+
+    # Make a prediction using the model
+    output = model(image)
+
+    # Get the index of the class with the highest probability
+    _, pred = torch.max(output, 1)
+
+
+    # Get the class name from the index using the ImageNet labels
+    imagenet_labels = torchvision.datasets.utils.download_url('https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt', '.', )
+    with open('imagenet_classes.txt') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    # Write top matches with probabilities in a log file with timestamp
+    timestamp = datetime.now().strftime("%Y%m%d_%H:%M:%S")
+    logfile.write('\U0001F4C5 ' + timestamp + '\n')
+    # look up top five matches and print them in the log file
+    N = 5
+    topN = torch.topk(output, N)
+    for i in range(N):
+        logfile.write(f'{emoji.emojize(":paw_prints:")} {classes[topN.indices[0][i]]}: {topN.values[0][i].item():.2%}\n')
+    logfile.write('\n')
+    # flush
+    logfile.flush()
+
+    # Print the result
+    print(f'The top predicted class is: {classes[topN.indices[0][0]]}')
+
+    # if 'cat' in top 5 classes, return True
+    if any('cat' in classes[topN.indices[0][i]] for i in range(N)):
+        return True
+    
+    return False
+
+
+
+# Function to detect motion
+def detect_motion(frame1, frame2):
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        else:
+            # Write log message with timestamp and motion detection area
+            logfile.write(f'{datetime.now().strftime("%Y%m%d_%H%M%S")} {emoji.emojize(":cat_face:", use_aliases=True)} Motion detected with {cv2.contourArea(contour)} \n')
+    return False
+
+if __name__ == '__main__':
+    # Parse the command-line arguments
+    # Define the parser
+    parser = argparse.ArgumentParser(description="Detect motion and check if a cat is present")
+    parser.add_argument('--rtsp_url', type=str, help='RTSP URL for the camera stream')
+    parser.add_argument('--topic', type=str, help='topic for ntfy')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    logfile = open('log.txt', 'a')
+
+    # Open the video
+    cap = cv2.VideoCapture(args.rtsp_url)
+
+    _, frame1 = cap.read()
+    _, frame2 = cap.read()
+    check_cat(frame1)
+
+    while cap.isOpened():
+        motion_detected = detect_motion(frame1, frame2)
+        timestamp = datetime.now().strftime("%Y%m%d_%H:%M:%S")
+
+        if motion_detected:
+            cv2.imwrite(f'motion_pics/{timestamp}.jpg', frame2)
+            if check_cat(frame2):
+                cv2.imwrite(f'cats/cat_detected_{timestamp}.jpg', frame2)
+                print("Cat detected and image saved!")
+                # ntfy alert with the image using requests
+                with open(f'cats/cat_detected_{timestamp}.jpg', 'rb') as f:
+                    r = requests.post(f'https://ntfy.sh/{args.topic}', 
+                                    data = {'title': 'Cat detected 🐈'},
+                                    files={'image': f})
+                # sleep 5 seconds
+                time.sleep(5)
+
+        frame1 = frame2
+        ret, frame2 = cap.read()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Cat Detector
+
+This is a Python script that detects motion and checks if a cat is present in the video stream. If a cat is detected, the script saves an image of the cat and sends a notification to the user using the ntfy service.
+
+## Requirements
+
+- Python 3.x
+- PyTorch
+- TorchVision
+- Pillow
+- OpenCV
+- Emoji
+- Requests
+
+## Installation
+
+1. Clone the repository:
+'pip install -r requirements.txt'
+
+2. Install the required packages:
+'pip install -r requirements.txt'
+
+3. Run the script:
+## Usage
+'python cat_detector.py --rtsp_url rtsp://username:password@ip_address:port/path'
+
+The script takes one command-line argument:
+
+- `--rtsp_url`: The RTSP URL for the camera stream.
+
+The script will write log messages to a file named `cat_detector.log` in the current directory. If a cat is detected, the script will save an image of the cat to the `cats` directory and send a notification to the user using the ntfy service.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
